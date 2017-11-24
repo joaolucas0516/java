ae006eb9202fd9e06d288e2d3a55749757996153
joaolucas0516 <32168994+joaolucas0516@users.noreply.github.com>
2017-11-24 15:14:41 +0000

    Add files via upload

---
+++ dhsd.java
@@ -0,0 +1,9 @@
+package diadasbruxas;
+
+public class dhsd {
+	public static void main(String[] args) {
+		final String mensagem = "Hoje é sexta feira";
+		for (int I=1 ; I<=5 ; I++)
+			System.out.println(mensagem);
+	}
+}
