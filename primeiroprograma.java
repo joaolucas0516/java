b83cdbbfb70a55d41bfeee03187e022785d8957f
joaolucas0516 <32168994+joaolucas0516@users.noreply.github.com>
2017-11-30 17:24:13 +0000

    Add files via upload

--- primeiroprograma.java
+++ primeiroprograma.java
@@ -3,8 +3,8 @@ package pacote1;
 public class primeiroprograma {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
+		// TODO Auto-generated method stub
+System.out.println("Ola Mundo");
 
-	}
 
-}
+
